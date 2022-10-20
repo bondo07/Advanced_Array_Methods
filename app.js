ea95367811f8a1e4feafce4c12ab1bebd6c66844
Armando Beltran <armando_b@live.com>
2022-10-20 14:24:14 -0500

    Problem Two, Three, & Four Finished

--- app.js
+++ app.js
@@ -139,11 +139,67 @@ let dishes = [
 //1. Create a function that will return all dishes with the cuisine type of "vegetarian"
 //Filter
 
-function problemOne(){
+// function problemOne(){
+
+//     let results;
+//     results = dishes.filter(function(el) {
+//         if(el.cuisine === "Vegetarian") {
+//             return true;
+//         }
+//         else {
+//             return false;
+//         }
+//     })
+//     return results;
+// }
+
+// let vegetarianFood = problemOne();
+// console.log(vegetarianFood)
+
+//2. Create a function that will prompt the user to enter a cuisine type and then return all dishes that match that type
+//Filter
+
+// function problemTwo() {
+//     let results;
+//     userPrompt = prompt("Please choose a cuisine type from the following; Italian, Mexican, French, Irish, Vegetarian, Hungarian, Vietnamese")
+//     results = dishes.filter(function(dish) {
+//         return userPrompt === dish.cuisine
+//     })
+//     return results;
+// }       
+// let userFood = problemTwo()
+// console.log(userFood)
+
+// 3. Create a function that will return all dishes with the cuisine type of "Italian" and a serving size greater than 5.
+// Filter
+
+// function problemThree(){
+
+//     let results;
+//     results = dishes.filter(function(dish) {
+//         if(dish.cuisine === "Italian" && dish.servings > 5) {
+//             return true;
+//         }
+//         else {
+//             return false;
+//         }
+//     })
+//     return results;
+// }
+
+// let ItalianFood = problemThree();
+// console.log(ItalianFood)
+
+
+
+//4. Create a function that will return only dishes whose id number matches their serving count.
+//Filter
+
+function problemFour () {
 
     let results;
-    results = dishes.filter(function(el) {
-        if(el.cuisine === "Vegetarian") {
+    results = dishes.filter(function(dish) {
+        if(dish.id === dish.servings) {
             return true;
         }
         else {
@@ -153,22 +209,8 @@ function problemOne(){
     return results;
 }
 
-let vegetarianFood = problemOne();
-console.log(vegetarianFood)
-//2. Create a function that will prompt the user to enter a cuisine type and then return all dishes that match that type
-//Filter
-
-
-
-//3. Create a function that will return all dishes with the cuisine type of "Italian" and a serving size greater than 5.
-//Filter
-
-
-
-//4. Create a function that will return only dishes whose id number matches their serving count.
-//Filter
-
-
+let idServingCount = problemFour();
+console.log(idServingCount)
 
 //5. Create a function that will return only dishes whose serving count is even.
 //Filter
